69698741f416e4cb1504457468a28a66ed647a88
Listochek <sataev.vl@gmail.com>
2024-02-06 21:15:40 +0700

    add save video

--- main/main.py
+++ main/main.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 from path_data import path_to_model
 from path_data import path_to_video
+from path_data import path_to_save
+from path_data import saver_videos
+
 from notification import send_email
 
-
+import time
 import cv2
 import cvzone
 import math
@@ -15,17 +18,27 @@ cap = cv2.VideoCapture(path_to_video)
 
 classNames = ['gun']
 # настройки видео
-cap.set(3, 1280)
-cap.set(4, 720)
+width_video = 1280
+height_video = 720
+
+cap.set(3, width_video)
+cap.set(4, height_video)
+
+
 
 def main():
+    if saver_videos == True:
+        size_video = (width_video, height_video)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(path_to_save, fourcc, 30.0, size_video)
     gun_counter = 0
     while True:
         success, img = cap.read()
-        results = model(img, stream=True)
+        results = model(img, stream=True, verbose=False, iou=0.4, conf=0.5)
     
         for r in results:
             boxes = r.boxes
+            
             for box in boxes:
                 x1, y1, x2, y2, = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
@@ -38,12 +51,20 @@ def main():
                 cvzone.putTextRect(img, f'{classNames[cls]}{conf}', (max(0, x1), max(0, y1)))
                 if classNames[cls] == 'gun':
                     gun_counter += 1
-                print(gun_counter)
-                if gun_counter == 20:
-                    send_email()
-                    gun_counter = 0
+               
                 
-        cv2.imshow('Image', img)
-        cv2.waitKey(1)
+                if gun_counter == 20:
+                    #send_email()
+                    gun_counter = 0
+            if saver_videos == True:
+                out.write(img)
+
+        try: 
+            cv2.imshow('Image', img)
+            cv2.waitKey(1)
+        except:
+            cap.release() 
+            cv2.destroyAllWindows()
+            return
 
 main()
--- main/path_data.py
+++ main/path_data.py
@@ -2,6 +2,11 @@
 
 """ ФАЙЛ ДЛЯ УДОБНОЙ РАБОТЫ С ПУТЯМИ"""
 
-
-path_to_model = 'models/nano_gun.pt'
-path_to_video = 'test_data/test_vide_1.mp4'
+# путь до модели которая будет обрабатывать видео
+path_to_model = 'models/medium_gun.pt' 
+# путь до видео
+path_to_video = 'test_data\\test_video_2.mp4'
+# Сохранять ли видео
+saver_videos = True
+#Путь для сохранения видео
+path_to_save = 'video_saves\\save_video_2.avi'
