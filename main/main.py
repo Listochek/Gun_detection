99601d8c75eecfe84bb727baf146d92f79279676
Pelbmenb <artem.jidov@gmail.com>
2024-02-15 21:36:18 +0700

    interface update

--- main/main.py
+++ main/main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QSlider
 from ultralytics import YOLO
 from path_data import path_to_model, path_to_video, path_to_save, saver_videos
 from notification import send_email
@@ -17,12 +17,29 @@ CONFIDENCE_THRESHOLD = 0.5
 
 class VideoWidget(QWidget):
     def __init__(self):
-        print("Starting video")
+        print("Gun_tetection>> Запуск класса...")
         super().__init__()
 
         self.label = QLabel(self)
         layout = QVBoxLayout(self)
         layout.addWidget(self.label)
+
+        self.detected_objects_label = QLabel(self)
+        layout.addWidget(self.detected_objects_label)
+
+        font = self.detected_objects_label.font()
+        font.setPointSize(16)
+        self.detected_objects_label.setFont(font)
+
+        self.confidence_slider = QSlider(Qt.Horizontal)
+        self.confidence_slider.setRange(0, 100)
+        self.confidence_slider.setValue(int(CONFIDENCE_THRESHOLD * 100))
+        self.confidence_slider.valueChanged.connect(self.update_confidence_threshold)
+        layout.addWidget(self.confidence_slider)
+
+        self.threshold_label = QLabel(self)
+        self.update_threshold_label()
+        layout.addWidget(self.threshold_label)
 
         self.cap = cv2.VideoCapture(path_to_video)
         self.cap.set(3, WIDTH_VIDEO)
@@ -34,6 +51,8 @@ class VideoWidget(QWidget):
 
         self.model = YOLO(path_to_model)
 
+        self.setWindowTitle("Gun_Detector")
+
     def update_frame(self):
         success, img = self.cap.read()
         if success:
@@ -42,7 +61,7 @@ class VideoWidget(QWidget):
                 size_video = (WIDTH_VIDEO, HEIGHT_VIDEO)
                 fourcc = cv2.VideoWriter_fourcc(*'XVID')
                 out = cv2.VideoWriter(path_to_save, fourcc, 30.0, size_video)
-                pass
+
 
     def process_frame(self, img):
         gun_counter = 0
@@ -55,16 +74,17 @@ class VideoWidget(QWidget):
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 w, h = x2 - x1, y2 - y1
 
-                cvzone.cornerRect(img, (x1, y1, w, h), colorR=(255, 100, 255), colorC=(200, 150, 255))
-                conf = math.ceil(box.conf[0] * 100) / 100
+                if box.conf[0] >= CONFIDENCE_THRESHOLD:
+                    cvzone.cornerRect(img, (x1, y1, w, h), colorR=(0, 0, 255), colorC=(0, 0, 255))
+                    conf = math.ceil(box.conf[0] * 100) / 100
 
-                cls = int(box.cls[0])
-                cvzone.putTextRect(img, f'{CLASS_NAMES[cls]} {conf}', (max(0, x1), max(0, y1)))
-                if CLASS_NAMES[cls] == 'gun':
-                    gun_counter += 1   
-                    if gun_counter == 20:
-                        #send_email()
-                        gun_counter = 0
+                    cls = int(box.cls[0])
+                    cvzone.putTextRect(img, f'{CLASS_NAMES[cls]} {conf}', (max(0, x1), max(0, y1)))
+                    if CLASS_NAMES[cls] == 'gun':
+                        gun_counter += 1
+
+
+            self.detected_objects_label.setText(f'Обнаружено: {gun_counter}')
 
             height, width, channel = img.shape
             bytes_per_line = 3 * width
@@ -73,9 +93,21 @@ class VideoWidget(QWidget):
             pixmap = QPixmap.fromImage(q_img)
             self.label.setPixmap(pixmap)
 
+    def update_confidence_threshold(self, value):
+        global CONFIDENCE_THRESHOLD
+        CONFIDENCE_THRESHOLD = value / 100.0
+        self.update_threshold_label()
+
+    def update_threshold_label(self):
+        font = self.threshold_label.font()
+        font.setPointSize(16)
+        self.threshold_label.setFont(font)
+        self.threshold_label.setText(f'Мин. порог: {self.confidence_slider.value()}%')
+
 if __name__ == '__main__':
+    print("Gun_tetection>> __main__ запущен!")
     app = QApplication(sys.argv)
-    print("logging video")
     window = VideoWidget()
     window.show()
     sys.exit(app.exec_())
+    print("Gun_tetection>> __main__ закончен!")
