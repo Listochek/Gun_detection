c2dbd3ebc41f643634265dc5cfab033b61e3325b
Pelbmenb <artem.jidov@gmail.com>
2024-02-15 21:50:52 +0700

    upd

--- main/path_data.py
+++ main/path_data.py
@@ -5,8 +5,8 @@
 # путь до модели которая будет обрабатывать видео
 path_to_model = 'models/medium_gun.pt' 
 # путь до видео
-path_to_video = 'test_data\\test_video_2.mp4'
+path_to_video = 'test_data\\test_video_1.mp4'
 # Сохранять ли видео
 saver_videos = True
 #Путь для сохранения видео
-path_to_save = 'video_saves\\save_video_2.avi'
+path_to_save = 'video_saves\\save_video_1.avi'
