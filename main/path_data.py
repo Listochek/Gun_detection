49f30badecaae0668aa90d3f5e6d89679c7f0617
Listochek <sataev.vl@gmail.com>
2024-04-04 20:49:59 +0700

    upd model

--- main/path_data.py
+++ main/path_data.py
@@ -3,7 +3,8 @@
 """ ФАЙЛ ДЛЯ УДОБНОЙ РАБОТЫ С ПУТЯМИ"""
 
 # путь до модели которая будет обрабатывать видео
-path_to_model = 'models/medium_gun.pt' 
+path_to_model = 'models/med_gun_200.pt' 
+
 # путь до видео
 path_to_videos = 'test_data\\' 
 path_to_video = 'test_data\\test_video_1.mp4'
